2527e3da556a73458639092e3264925fc3f6938d
Craig Christenson <cchristenson@2co.com>
2014-04-29 21:23:08 -0400

    * Added method to set each credential and option individually in place of setCredentials and setApiCredentials methods. * Cleaned up whitespace and indenting. * Changed a few method names to use camel case for consistency. * Replaced "environment" option with "verifySSL" option for clarity. * Refactored Admin API and Payment API request methods to one method. * Change default response format to Array instead of JSON.

--- test/NotificationTest.php
+++ test/NotificationTest.php
@@ -5,17 +5,17 @@ require_once(dirname(__FILE__) . '/../lib/Twocheckout.php');
 class TestNotification extends PHPUnit_Framework_TestCase
 {
 
-  public function testNotificationCheck()
-  {
-    $params = array(
-    'vendor_id' => '1817037',
-    'sale_id' => '4774380224',
-    'invoice_id' => '4774380233',
-    'md5_hash' => '566C45D68B75357AD43F9010CFFE8CF5',
-    'secret' => 'tango'
-    );
-    $result = Twocheckout_Notification::check($params, 'tango', 'array');
-    $this->assertEquals("Success", $result['response_code']);
-  }
+    public function testNotificationCheck()
+    {
+        $params = array(
+            'vendor_id' => '1817037',
+            'sale_id' => '4774380224',
+            'invoice_id' => '4774380233',
+            'md5_hash' => '566C45D68B75357AD43F9010CFFE8CF5',
+            'secret' => 'tango'
+        );
+        $result = Twocheckout_Notification::check($params, 'tango');
+        $this->assertEquals("Success", $result['response_code']);
+    }
 
 }
